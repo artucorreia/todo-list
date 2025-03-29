d7b802eea68efd17724e0a5aff056334709097d3
artucorreia <arthurcorria0109@gmail.com>
2025-03-28 21:17:44 -0300

    feat: add tailwind css

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- Tailwind css -->
+    @vite('resources/css/app.css')
+
     <title>@yield('title')</title>
 
     <!-- Google fonts -->
